9ef6db0339861360287101111c04791cd3c802f8
worldfish <811361873@qq.com>
2019-08-09 16:24:57 +0800

    add check cookies

--- src/main/java/com/example/community/controller/AuthorizeController.java
+++ src/main/java/com/example/community/controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller//把当前类作为路由API的承载者
@@ -33,7 +35,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request){
+                           HttpServletRequest request,
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id("clientId");
         accessTokenDTO.setClient_secret("clientSecret");
@@ -43,14 +46,17 @@ public class AuthorizeController {
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = GithubProvider.getUser(accessToken);
         //System.out.println(user.getName());
+        //github登录成功后，把用户信息插入到数据库中，并把token设置cookie
         if(githubUser != null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
 
             //登录成功，写cookie,和session
             request.getSession().setAttribute("user",githubUser);//把user的值赋值给session
--- src/main/java/com/example/community/mapper/UserMapper.java
+++ src/main/java/com/example/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.example.community.mapper;
 import com.example.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 
 @Mapper
@@ -10,4 +12,6 @@ public interface UserMapper {
     @Insert("insert into user (ACCOUNT_ID,NAME,TOKEN,GMT_CREATE,GMT_MODIFIED) value (#{accountId} ,#{name},#{token} ,#{gmtCreate} ,#{gmtModified} )")
     void insert(User user);
 
+    @Select("select * from user where token = #{token} ")
+    User findByToken(@Param("token") String token);
 }
